879ce846427d874d1ce4abc191ac4da58381c7d0
Nikita Serdashenko <nserdashenko@eyeconweb.com>
2023-09-16 17:01:35 +0400

    grpc codes formatting to custom

--- grpc_error_handler.go
+++ grpc_error_handler.go
@@ -3,6 +3,7 @@ package error
 import (
 	"errors"
 	customError "github.com/nsdash/go-error-lib"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
@@ -27,6 +28,22 @@ func (h GrpcErrorHandler) parseError(statusErr *status.Status) customError.Error
 	errorCode := statusErr.Code()
 	errorMessage := statusErr.Message()
 
-	applicationError := customError.NewApplicationError(errors.New(errorMessage), int(errorCode))
+	applicationError := customError.NewApplicationError(errors.New(errorMessage), h.grpcErrorToCustom(errorCode))
 	return &applicationError
+}
+
+func (h GrpcErrorHandler) grpcErrorToCustom(code codes.Code) int {
+	if code == codes.InvalidArgument {
+		return customError.CodeFieldIsInvalid
+	}
+
+	if code == codes.NotFound {
+		return customError.CodeFieldEntityNotFound
+	}
+
+	if code == codes.AlreadyExists {
+		return customError.CodeFieldIsUnique
+	}
+
+	return customError.CodeFieldUnauthorized
 }
